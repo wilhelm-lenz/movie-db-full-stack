85b4b245edf9127c1d81fc5643b480ec62566b6e
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2024-02-04 15:43:04 +0100

    refactoring

--- backend/src/domain/Movies.js
+++ backend/src/domain/Movies.js
@@ -17,35 +17,35 @@ exports.makeMovie = ({
   awards,
   type,
 }) => {
-  // if (!genres || !Array.isArray(genres) || genres.length === 0) {
-  //   throw new Error("At least one genre required");
-  // }
+  if (!genres || !Array.isArray(genres) || genres.length === 0) {
+    throw new Error("At least one genre required");
+  }
 
-  // console.log("Title: ", typeof title);
-  // if (
-  //   typeof title === "undefined" ||
-  //   typeof title !== "string" ||
-  //   title.trim() === ""
-  // ) {
-  //   throw new Error("Title Value is invalid!");
-  // }
-  // console.log("year: ", typeof year);
-  // if (typeof year === "undefined" || typeof year !== "number" || year === 0) {
-  //   throw new Error("The Year Value is invalid!");
-  // }
-  // console.log("director: ", typeof director);
-  // if (
-  //   typeof director === "undefined" ||
-  //   typeof director !== "string" ||
-  //   director.trim() === ""
-  // ) {
-  //   throw new Error("Director Value is invalid!");
-  // }
+  console.log("Title: ", typeof title);
+  if (
+    typeof title === "undefined" ||
+    typeof title !== "string" ||
+    title.trim() === ""
+  ) {
+    throw new Error("Title Value is invalid!");
+  }
+  console.log("year: ", typeof year);
+  if (typeof year === "undefined" || typeof year !== "number" || year === 0) {
+    throw new Error("The Year Value is invalid!");
+  }
+  console.log("director: ", typeof director);
+  if (
+    typeof director === "undefined" ||
+    typeof director !== "string" ||
+    director.trim() === ""
+  ) {
+    throw new Error("Director Value is invalid!");
+  }
 
-  // console.log("genres: ", typeof genres);
-  // if (!Array.isArray(genres) || genres.length === 0) {
-  //   throw new Error("At least one genre required");
-  // }
+  console.log("genres: ", typeof genres);
+  if (!Array.isArray(genres) || genres.length === 0) {
+    throw new Error("At least one genre required");
+  }
 
   // // // const tomatoRating =
   // // //   tomato && typeof tomato.rating === "number" ? tomato.rating : null;
@@ -55,40 +55,40 @@ exports.makeMovie = ({
   // // //   throw new Error("Rating Value is invalid!");
   // // // }
 
-  // // // if (
-  // // //   (tomato && typeof tomato.rating === "undefined") ||
-  // // //   typeof tomato.rating !== "number"
-  // // // ) {
-  // // //   throw new Error("Rating Value is invalid!");
-  // // // }
+  if (
+    (tomato && typeof tomato.rating === "undefined") ||
+    typeof tomato.rating !== "number"
+  ) {
+    throw new Error("Rating Value is invalid!");
+  }
 
-  // // console.log("poster: ", typeof poster);
-  // // if (
-  // //   typeof poster === "undefined" ||
-  // //   typeof poster !== "string" ||
-  // //   !poster.includes("http") ||
-  // //   poster.trim() === ""
-  // // ) {
-  // //   throw new Error("Poster Url Value is invalid!");
-  // // }
+  console.log("poster: ", typeof poster);
+  if (
+    typeof poster === "undefined" ||
+    typeof poster !== "string" ||
+    !poster.includes("http") ||
+    poster.trim() === ""
+  ) {
+    throw new Error("Poster Url Value is invalid!");
+  }
 
-  // console.log("plot: ", typeof plot);
-  // if (
-  //   typeof plot === "undefined" ||
-  //   typeof plot !== "string" ||
-  //   plot.trim() === ""
-  // ) {
-  //   throw new Error("Description Value is invalid!");
-  // }
+  console.log("plot: ", typeof plot);
+  if (
+    typeof plot === "undefined" ||
+    typeof plot !== "string" ||
+    plot.trim() === ""
+  ) {
+    throw new Error("Description Value is invalid!");
+  }
 
-  // console.log("runtime: ", typeof runtime);
-  // if (
-  //   typeof runtime === "undefined" ||
-  //   typeof runtime !== "number" ||
-  //   runtime === 0
-  // ) {
-  //   runtime = 90;
-  // }
+  console.log("runtime: ", typeof runtime);
+  if (
+    typeof runtime === "undefined" ||
+    typeof runtime !== "number" ||
+    runtime === 0
+  ) {
+    runtime = 90;
+  }
 
   const movie = {
     _id,
